cddf0e48efac4f5f16c864238f6ae55b2fbfdfa1
eddiemac1993 <manjoloe800@gmail.com>
2025-10-05 03:47:47 +0000

    5th october

--- fertilizer_mgmt/urls.py
+++ fertilizer_mgmt/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,3 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('fertilizer_tracking.urls')),  # your app urls
 ]
+
+# This serves static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
